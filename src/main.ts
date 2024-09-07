69d14e007f3258121fee204b50368a7fcf608ab8
kangshuisheng <2538041184@qq.com>
2024-09-07 09:24:34 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      preflightContinue: false,
-      optionsSuccessStatus: 200,
-    }),
-  );
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
